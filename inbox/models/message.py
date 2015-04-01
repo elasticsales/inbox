f63debb98a21f054478d8b4991413cdf6acb2e40
Thomas Steinacher <tom@eggdrop.ch>
2015-04-01 14:22:12 +0200

    Remove duplicate code

--- inbox/models/message.py
+++ inbox/models/message.py
@@ -207,13 +207,6 @@ class Message(MailSyncBase, HasRevisions, HasPublicID):
             msg.namespace_id = account.namespace.id
             parsed = mime.from_string(body_string)
 
-            from inbox.models.block import Block, Part
-            body_block = Block()
-            body_block.namespace_id = account.namespace.id
-            body_block.data = body_string
-            body_block.content_type = "text/plain"
-            msg.full_body = body_block
-
             mime_version = parsed.headers.get('Mime-Version')
             # sometimes MIME-Version is '1.0 (1.0)', hence the .startswith()
             if mime_version is not None and not mime_version.startswith('1.0'):
