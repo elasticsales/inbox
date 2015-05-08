0310f450d8971f7658b8141032d6655f72b4e44b
Thomas Steinacher <tom@eggdrop.ch>
2015-05-08 18:30:00 +0200

    Adding message_id_header index for debugging purposes

---
+++ migrations/versions/163_add_message_id_header_idx.py
@@ -0,0 +1,22 @@
+"""Add message_id_header index
+
+Revision ID: 1b0b4e6fdf96
+Revises: 4a8aacb82076
+Create Date: 2015-05-08 16:25:39.140215
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1b0b4e6fdf96'
+down_revision = '4a8aacb82076'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+def upgrade():
+    op.create_index('ix_message_message_id_header', 'message', ['message_id_header'], unique=False)
+
+
+def downgrade():
+    op.drop_index('ix_message_message_id_header', table_name='message')
