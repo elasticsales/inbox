7b8bf4f23ed47808bc4eb75e093169b08459578f
Thomas Steinacher <tom@eggdrop.ch>
2015-06-20 21:49:38 +0800

    Make sure alive is False if we have no folders in heartbeat

--- inbox/api/metrics_api.py
+++ inbox/api/metrics_api.py
@@ -61,7 +61,7 @@ def index():
             if account.id in heartbeat:
                 account_heartbeat = heartbeat[account.id]
                 account_folder_data = folder_data[account.id]
-                alive = True
+                alive = bool(account_heartbeat.folders)
                 for folder_status in account_heartbeat.folders:
                     folder_status_id = int(folder_status.id)
                     if folder_status_id in account_folder_data:
